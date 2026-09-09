092ba2dde68458007b2455836f3caa30dde7d3db
Juan Camilo Gómez <jcgomezj@eafit.edu.co>
2026-09-09 07:19:41 -0500

    style: resolver errores de ruff en casos prueba bdd

--- coinogas_project/urls.py
+++ coinogas_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+
 from core.views import formulario_requerimiento
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', formulario_requerimiento, name='formulario_en_blanco'),
+    path("admin/", admin.site.urls),
+    path("", formulario_requerimiento, name="formulario_en_blanco"),
 ]
